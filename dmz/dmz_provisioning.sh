3dd9100fdc3f1e66eac0910f7ef3ff9e3915fdf3
lwheagar24 <55165106+lwheagar24@users.noreply.github.com>
2019-10-01 18:46:44 -0400

    Add files via upload

---
+++ dmz/dmz_provisioning.sh
@@ -0,0 +1,26 @@
+#download vm
+#vagrant box add ubuntu/bionic64
+#initialize vm
+#vagrant init ubuntu/bionic64
+#power on vm
+#vagrant up
+#ssh into vm
+#vagrant ssh
+#download updates in vm
+sudo apt-get update
+#install updates in vm
+sudo apt-get upgrade
+#change hostname in vm
+sudo hostname DMZ
+#power off vm
+#vagrant halt
+
+#set vm network bridged in virtual box
+
+#set ip in vm
+
+#set subnetmask in vm
+
+#set default gateway in vm 
+
+#set broadcast in vm
