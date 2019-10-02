c706f65cbce7f2c9b86ef4d623d93f4ffb6add0e
thetarkus <mike@mikemob.com>
2019-10-02 16:22:09 -0400

    Add environment varables file for consumers

--- db/consumers/log_consumer.py
+++ db/consumers/log_consumer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from consumer import Consumer
 from logger import Logger
 
@@ -6,13 +7,12 @@ from logger import Logger
 if __name__ == '__main__':
     # Config
     # TODO: Use environment variables
-    host = ''
-    port = 5672
-    queue = 'log-queue'
-    username = 'log'
-    password = 'logpass'
-    log_path = '/var/log/car-calendar'
-    logger = Logger(log_path)
+    host = str(os.getenv('RABBITMQ_HOST'))
+    port = int(os.getenv('RABBITMQ_PORT', 5672))
+    queue = os.getenv('RABBITMQ_QUEUE', 'log-queue')
+    username = os.getenv('RABBITMQ_USER', 'log')
+    password = os.getenv('RABBITMQ_PASS', 'logpass')
+    logger = Logger(os.getenv('LOG_PATH', '/var/log/car-calendar'))
 
     def callback(ch, method, properties, body):
         logger.write_log('LOG', str(body))
