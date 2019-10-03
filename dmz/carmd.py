c94402915dfba4b91daec6f47c3a8c48c3b772c5
lwheagar24 <55165106+lwheagar24@users.noreply.github.com>
2019-10-03 11:38:22 -0400

    Update carmd.py

--- dmz/carmd.py
+++ dmz/carmd.py
@@ -1,10 +1,12 @@
+import sys
 import requests
-#vin = "vin=1GNALDEK9FZ108495"
-mileage = "mileage=50000"
-year = "year=2002"
-make = "make=Ford"
-model = "model=Focus"
-#api_endpoint = "http://api.carmd.com/v3.0/maint?%s&%s" %(vin, mileage)
+if len(sys.argv) < 5:
+    print("syntax:\npython carmd.py YEAR MAKE MODEL MILEAGE")
+    quit()
+year = "year=" + sys.argv[1]
+make = "make=" + sys.argv[2]
+model = "model=" + sys.argv[3]
+mileage = "mileage=" + sys.argv[4]
 api_endpoint = "http://api.carmd.com/v3.0/maint?%s&%s&%s&%s" %(year, make, model, mileage)
 header = {"content-type":"application/json", "authorization":"Basic MGE2OTJlMWQtY2M5YS00OWMwLTlmYTItNzNjZGFjMjYyZjBm", "partner-token":"8205959faed74cbcb946419b79e80a87"}  
 request = requests.get(url = api_endpoint, headers = header)
