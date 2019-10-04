1877a299ca339e47f87153892102cdd27f92bf0b
thetarkus <mike@mikemob.com>
2019-10-04 11:49:00 -0400

    Setup flask with templates

--- web/src/main.py
+++ web/src/main.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/login')
+def login():
+    data = {
+        'example': 'This is how to pass a variable to a template.'
+    }
+    return render_template('login.html', **data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
