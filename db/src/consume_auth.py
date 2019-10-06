e869655850129d35c86b2dac3949badb664f4a87
thetarkus <mike@mikemob.com>
2019-10-05 20:43:30 -0400

    Add complete amqp auth consumer

--- db/src/consume_auth.py
+++ db/src/consume_auth.py
@@ -1,32 +1,64 @@
 #!/usr/bin/env python3
 import os
-import dotenv
-from consumers.auth_consumer import AuthConsumer  # pylint: disable=import-error
+import json
+from amqp.consumer import Consumer  # pylint: disable=import-error
+from database.auth import Auth  # pylint: disable=import-error
 from dotenv import load_dotenv
 
 
 def callback(ch, method, props, body):
     """
-
+    Auth callback that is called when an auth attempt occurs.
     """
-    print('Received:', body)
+    try:
+        data = json.loads(body)
+    except:
+        return  # Malformed JSON
+
+    if not data:
+        return
+
+    action = data.get('action')
+    result = { 'success': False }
+
+    # Received login attempt
+    if action == 'login':
+        result = Auth.login(
+            username_or_email=data.get('username') or data.get('email'),
+            password=data.get('password')
+        )
+
+    # Received registration attempt
+    elif action == 'register':
+        result = Auth.register(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=data.get('password'),
+            first_name=data.get('first_name'),
+            last_name=data.get('last_name')
+        )
+
+    else:
+        result['message'] = 'Unknown action.'
+
+    return json.dumps(result)
 
 
 def main():
     """
-
+    Start auth consumer.
     """
     load_dotenv(os.getenv('AUTH_ENV', '.env_auth'))  # Load .env file
 
     # Start consuming
-    auth_consumer = AuthConsumer(
+    auth_consumer = Consumer(
         host=os.getenv('RABBITMQ_HOST'),
         port=int(os.getenv('RABBITMQ_PORT', 5672)),
-        vhost=os.getenv('RABBITMQ_VHOST'),
+        vhost=os.getenv('RABBITMQ_VHOST', '/'),
         username=os.getenv('RABBITMQ_USER'),
         password=os.getenv('RABBITMQ_PASS')
     )
-
+    print('[*] Waiting for auth messages. To exit press CTRL+C')
     auth_consumer.consume(
         queue=os.getenv('RABBITMQ_QUEUE', 'auth-queue-rpc'),
         callback=callback
