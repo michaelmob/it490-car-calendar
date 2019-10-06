08ec94a20d42e1fc36aa4982d4823e242e69343b
thetarkus <mike@mikemob.com>
2019-10-05 20:33:48 -0400

    Allow first_name and last_name to be null

--- db/sql/users.sql
+++ db/sql/users.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(128) NOT NULL,
-  `last_name` varchar(128) NOT NULL,
+  `first_name` varchar(128) NULL,
+  `last_name` varchar(128) NULL,
   `email` varchar(256) NOT NULL,
   `username` varchar(256) NOT NULL,
   `password` varchar(256) NOT NULL,
   `salt` varchar(256) NOT NULL,
   `token` varchar(128) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `unique_email` UNIQUE (`email`),
-  CONSTRAINT `unique_username` UNIQUE (`username`),
-  CONSTRAINT `unique_token` UNIQUE (`token`)
+  UNIQUE KEY `unique_email` (`email`),
+  UNIQUE KEY `unique_username` (`username`),
+  UNIQUE KEY `unique_token` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
