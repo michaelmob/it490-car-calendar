8ab54d4439afd8069085dbc94f1b3a7546b640c6
Luwai Wheagar <lgwheagar24@gmail.com>
2019-10-14 09:03:11 -0400

    DMZ Provision Script Changes

--- dmz/provision-dmz.sh
+++ dmz/provision-dmz.sh
@@ -6,6 +6,7 @@ apt-get upgrade
 apt-get install python3
 apt-get install python3-pip
 apt-get install python3.6
+apt-get install python3.6-pip
 pip install googleapiclient
 pip install httplib2
 pip install oauth2client
