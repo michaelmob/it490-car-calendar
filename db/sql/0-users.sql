6add424a110ecfb77f54dbf231d4975a9bc78ddc
thetarkus <mike@mikemob.com>
2019-10-14 11:02:51 -0400

    Add cars table, add datetimes for user, add numeric priorities

--- db/sql/0-users.sql
+++ db/sql/0-users.sql
@@ -1,13 +1,15 @@
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(128) NULL,
-  `last_name` varchar(128) NULL,
+  `first_name` varchar(128) DEFAULT NULL,
+  `last_name` varchar(128) DEFAULT NULL,
   `email` varchar(256) NOT NULL,
   `username` varchar(256) NOT NULL,
   `password` varchar(256) NOT NULL,
   `salt` varchar(256) NOT NULL,
   `token` varchar(128) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_email` (`email`),
   UNIQUE KEY `unique_username` (`username`),
