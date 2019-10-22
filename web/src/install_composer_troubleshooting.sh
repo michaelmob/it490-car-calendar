a4ec26faa003e597eb40ff50798a5f92633890d6
thetarkus <mike@mikemob.com>
2019-10-22 19:06:58 -0400

    web: Remove PHP leftover files

--- web/src/install_composer_troubleshooting.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install composer		#installs composer
-sudo chown -R $USER .composer		#give current user ownership of the hidden .composer direcotry
-sudo chmod 777 .composer		#give all permissions for the hidden composer directory exists either in current working directory or the home directory ~/.composer
-sudo apt install php7.2-bcmatch		#installs php7.2-bcmatch dependency
-sudo chmod -R 777 < pwd			#give all permissions for the current working directory
-composer require php-amqplib/php-amqplib #implements the php-amqplib into composer
