b51f2af23a683e6cd9b5e78de4bde0adbdecc049
Krushna Patel <kp592@njit.edu>
2019-10-22 20:16:37 -0400

    Update 1-cars.sql

--- db/sql/1-cars.sql
+++ db/sql/1-cars.sql
@@ -1,3 +1,10 @@
+-- Adminer 4.7.4 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE `cars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -5,10 +12,14 @@ CREATE TABLE `cars` (
   `make` varchar(128) NOT NULL,
   `model` varchar(128) NOT NULL,
   `year` int(11) NOT NULL,
-  `mileage` int(11) NOT NULL,
+  `current_mileage` int(11) NOT NULL,
+  `weekly_mileage` int(11) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2019-10-23 00:13:56
