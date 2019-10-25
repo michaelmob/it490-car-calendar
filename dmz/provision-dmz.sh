a9e66aba589502198945953ac92cafae4e613fe5
thetarkus <mike@mikemob.com>
2019-10-25 00:30:02 -0400

    Clean up dmz provisioner, add logs synced dir

--- dmz/provision-dmz.sh
+++ dmz/provision-dmz.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+# Update package list and install required packages
 apt-get update -y
-apt-get upgrade -y
-apt-get install python3 -y 
-apt-get install python3-pip -y 
-apt-get install python3.6 -y 
-pip3 install google-api-python-client
-pip3 install google_auth_oauthlib
-pip3 install httplib2
-pip3 install oauth2client
-pip3 install datetime
-pip3 install bs4
-pip3 install /opt/packages/amqp
-pip3 install /opt/packages/logger
-pip3 install python-env
+apt-get install -y python3 python3-pip
+
+# Install pip packages
+pip3 install -r /srv/car-calendar/requirements.txt
+
+# Setup permissions on logs
 mkdir -p /var/log/car-calendar
 chown -R vagrant:syslog /var/log/car-calendar
